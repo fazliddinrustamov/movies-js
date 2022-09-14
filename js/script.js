aa6d28752fed2d5e61a3cc2f3f6c8f474ddb2f35
fazliddinrustamov <azizyuldashev19800@gmail.com>
2022-09-14 15:47:14 +0500

    push full project!

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ let movieRes = $("#template-movies-js").content;
 let searchForm = $(".wrapper-form-js");
 let searchPlace = $(".search-movie-js", searchForm);
 
+let elModalMovie = $(".js-modal-movie");
+let elRatingInput = $(".rating-js");
+
 let movieInfo = [];
 
 let filterArray = movies.forEach((info) => {
@@ -15,6 +18,7 @@ let filterArray = movies.forEach((info) => {
     summary: info.summary,
     img: `https://i3.ytimg.com/vi/${info.ytid}/maxresdefault.jpg`,
     rating: info.imdb_rating,
+    imdbId: info.imdb_id,
     runtime: info.runtime,
     lang: info.language,
     ytLink: `https://www.youtube.com/watch?v=${info.ytid}`,
@@ -28,17 +32,16 @@ let renderMovies = function (movieInfo) {
   movieInfo.forEach((movie) => {
     let elCloneMovie = movieRes.cloneNode(true);
     
+    elCloneMovie.querySelector(".js-result-item").dataset.movieId = movie.imdbId;
     elCloneMovie.querySelector(".movie-img-js").src = movie.img;
     elCloneMovie.querySelector(".movie-img-js").alt = movie.title;
     elCloneMovie.querySelector(".movie-img-js").width = 250;
     elCloneMovie.querySelector(".movie-img-js").height = 200;
     elCloneMovie.querySelector(".movie-title-js").textContent = movie.title;
-    elCloneMovie.querySelector(".movie-subtitle-js").textContent = movie.fulltitle;
-    elCloneMovie.querySelector(".movie-year-js").textContent = `Year: ${movie.year}`;
-    elCloneMovie.querySelector(".movie-category-js").textContent = `Category: ${movie.category}`;
-    elCloneMovie.querySelector(".movie-rating-js").textContent = `Rating: ${movie.rating}`;
     elCloneMovie.querySelector(".movie-runtime-js").textContent = `Runtime: ${movie.runtime}`;
     elCloneMovie.querySelector(".movie-language-js").textContent = `Language: ${movie.lang}`;
+    elCloneMovie.querySelector(".movie-year-js").textContent = `Year: ${movie.year}`;
+    elCloneMovie.querySelector(".movie-rating-js").textContent = `Rating: ${movie.rating}`;
     elCloneMovie.querySelector(".movie-ytlink-js").href = movie.ytLink;
     elCloneMovie.querySelector(".movie-ytlink-js").textContent = "Trailer";
 
@@ -48,9 +51,9 @@ let renderMovies = function (movieInfo) {
   movieList.appendChild(elMovieWrapper);
 }
 
-let findMovies = (title) => {
+let findMovies = (title, minimumRating) => {
   return movieInfo.filter(movie => {
-    return movie.title.match(title);
+    return movie.title.match(title) && movie.rating > minimumRating;
   })
 }
 
@@ -61,14 +64,30 @@ searchForm.addEventListener('submit', function(evt){
 
   let searchPlaceConfigured = searchPlace.value.trim();
   let searchingRegex = new RegExp(searchPlaceConfigured, 'gi');
+  let minRating = Number(elRatingInput.value)
 
-  let searchResults = findMovies(searchingRegex);
+  let searchResults = findMovies(searchingRegex, minRating);
 
   movieSearchArr.push(searchResults);
   
   searchPlace.value = '';
+  elRatingInput.value = '';
 
   renderMovies(searchResults);
 });
 
-renderMovies(movieInfo.slice(0, 50));
+renderMovies(movieInfo.slice(50, 71));
+
+movieList.addEventListener('click', evt => {
+  if(evt.target.matches('.js-movie-info-button')){
+    let filmId = evt.target.closest(".js-result-item").dataset.movieId;
+    
+    
+    let foundFilms = movieInfo.find(film => {
+      return film.imdbId === filmId;
+    })
+
+    $('.js-modal-movie-title', elModalMovie).textContent = foundFilms.title;
+    $('.js-modal-movie-summary', elModalMovie).textContent = foundFilms.summary;
+  }
+})
